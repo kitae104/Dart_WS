8d79587b9d9b330ae54566b66147c7fefca2030c
Kitae's Github <aqua0405@gmail.com>
2022-09-05 01:49:25 +0900

    Update dart_file.dart

--- Dart_Basic/Test/dart_file.dart
+++ Dart_Basic/Test/dart_file.dart
@@ -1,23 +1,37 @@
 class MyHomePage extends StatelessWidget {
 
+  final _items = List.generate(50, (i) => ListTile(title: Text('No. $i')));
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Card"),
+        title: Text("SliverAppBar/SliverFillRemaining"),
       ),
-      body: Center(
-        child: Card(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(30.0),
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            pinned: true,   // 축소시 상단에 AppBar가 고정되는지 설정
+            expandedHeight: 180.0,  // 헤더의 최대 높이
+            flexibleSpace: FlexibleSpaceBar(  // 늘어나는 영역의 UI 정의
+              title: Text("Sliver"),
+              background: Image.asset('images/sample.jpg',
+                  fit: BoxFit.cover
+              ),
+            ),
+            actions: [
+              IconButton(
+                onPressed: (){
+
+                },
+                icon: Icon(Icons.arrow_downward),
+              ),
+            ],
           ),
-          elevation: 8.0,
-          child: Icon(
-            Icons.face,
-            color: Colors.grey,
-            size: 200,
+          SliverList(
+            delegate: SliverChildListDelegate(_items),
           ),
-        ),
+        ],
       ),
     );
   }
